5e707f12fda6185e4a2d516a8036450dcc7521e0
nattaporn2540 <giftsy389@gmail.com>
2018-06-26 12:17:08 +0700

    Initial commit

---
+++ src/mycode.java
@@ -0,0 +1,23 @@
+package  lab1;
+public class mycode {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("nattaporn sukying");
+        System.out.println("361211760008");
+        System.out.println("MIT211");
+        System.out.println("RMUTSV");
+
+
+
+
+    }
+
+
+
+
+
+
+}
